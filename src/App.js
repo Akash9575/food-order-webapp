e743eea18a1b69528f0f5f8e6f896155c8888b00
abhay.parsaniya <abhay.parsaniya@bacancy.com>
2022-06-15 17:52:53 +0530

    Restaurant Register Screen Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./component/NavBar";
 import "./App.css";
-import Register_Restaurant from "./component/Register-Restaurant";
+import RegisterRestaurant from "./component/RegisterRestaurant";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<NavBar />} />
         <Route path="/about" element={<NavBar />} />
         <Route path="/contact" element={<NavBar />} />
-        <Route path="/registerrestaurant" element={<Register_Restaurant/>} />
+        <Route path="/registerrestaurant" element={<RegisterRestaurant/>} />
         <Route path="/login" element={<NavBar />} />
         <Route path="/signup" element={<NavBar />} />
       </Routes>
