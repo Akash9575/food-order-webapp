1c4aa73d333c3663b50804d1caa97e87d4b00019
abhay.parsaniya <abhay.parsaniya@bacancy.com>
2022-06-19 14:47:56 +0530

    Pending Request Screen Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import PendingRequest from './components/PendingRequest';
 
 function App() {
   return (
     <div className="App">
+      <PendingRequest />
     </div>
   );
 }
